fde937bd52fb36810fb31021fb60b7f639ae90a3
MeiChang <uj99850@gmail.com>
2022-05-19 23:09:18 +0800

    test: use spyon to fetch cycling data

---
+++ src/api/__mocks__/tdxService.js
@@ -0,0 +1,5 @@
+import { vi } from 'vitest';
+
+export default {
+  getCyclingData: vi.fn()
+};
--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { setActivePinia, createPinia } from 'pinia';
-import { userDevice, userLocation } from './index.js';
+import { userDevice, userLocation } from '@/store';
 
 describe('store', () => {
   beforeEach(() => {
