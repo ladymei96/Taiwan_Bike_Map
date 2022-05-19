0d081a0cd8c359731082d8a35bf6999e447407ef
MeiChang <uj99850@gmail.com>
2022-05-19 22:52:09 +0800

    test: unit testing a store

---
+++ src/store/store.spec.js
@@ -0,0 +1,25 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { setActivePinia, createPinia } from 'pinia';
+import { userDevice, userLocation } from './index.js';
+
+describe('store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('test user device', async () => {
+    const deviceStore = userDevice();
+    expect(deviceStore.isMobile).toBe(false);
+    deviceStore.$patch({ isMobile: true });
+    expect(deviceStore.isMobile).toBe(true);
+  });
+  it('test user location', () => {
+    const geoLocationStore = userLocation();
+    const geolocation = {
+      latitude: 121.29042,
+      longitude: 25.00722
+    };
+    geoLocationStore.$patch({ geolocation });
+    expect(geoLocationStore.geolocation).toEqual(geolocation);
+  });
+});
