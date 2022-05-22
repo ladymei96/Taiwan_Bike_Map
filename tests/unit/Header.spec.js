e451f1c01a6d389f8fd105d192a7749c2efe2870
MeiChang <uj99850@gmail.com>
2022-05-22 23:37:51 +0800

    test: using a real router to disappear Vue warn

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -1,14 +1,24 @@
 import { describe, expect, it } from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import Header from '/src/components/common/Header.vue';
+
 import { createTestingPinia } from '@pinia/testing';
 import { userDevice } from '@/store';
+
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { setupLayouts } from 'virtual:generated-layouts';
+import generatedRoutes from 'virtual:generated-pages';
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: setupLayouts(generatedRoutes)
+});
 
 describe('Header', () => {
   it('is mobileLogo exists', async () => {
     const wrapper = shallowMount(Header, {
       global: {
-        plugins: [createTestingPinia()]
+        plugins: [createTestingPinia(), router]
         /** When using vitest with globals: true
          * if globals: false, need to provide a createSpy option
          * createTestingPinia({
